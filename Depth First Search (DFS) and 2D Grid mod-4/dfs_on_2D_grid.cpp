639e4d58f5c62500e4b885d6c0a8f495da8e82ec
raihanuldev <rihanthedev@gmail.com>
2025-02-05 08:23:32 +0600

    dfs-on-2d-grid-implementation

--- Depth First Search (DFS) and 2D Grid mod-4/dfs_on_2D_grid.cpp
+++ Depth First Search (DFS) and 2D Grid mod-4/dfs_on_2D_grid.cpp
@@ -3,24 +3,29 @@ using namespace std;
 char grid[105][105];
 bool vis[105][105];
 vector<pair<int,int>>d = {{-1,0},{1,0},{0,-1},{0,1}};
+int n,m;
 
+bool isVaild(int i,int j)
+{
+    if(i<0 || i>=n || j<0|| j>=m)
+        return false;
+    return true;
+}
 void dfs(int si,int sj)
 {
-    // cout<<si<<" "<<sj;
+    cout<<si<<" "<<sj<<endl;
     vis[si][sj] = true;
-    // source note theke left,right,up,down dhike jaoa jai tai ei ghula bahir korbo ekn
     for(int i =0; i<4; i++)
     {
-        int ci,cj;
-        ci =si+ d[i].first;
-        cj = sj+ d[i].second;
-        cout<<ci<<" "<<cj<<endl;
+        int ci =si+ d[i].first;
+        int cj = sj+ d[i].second;
+        if(isVaild(ci,cj) && !vis[ci][cj])
+            dfs(ci,cj);
     }
 }
 
 int main()
 {
-    int n,m;
     cin>>n>>m;
     
     for(int i = 0; i<n;i++)
