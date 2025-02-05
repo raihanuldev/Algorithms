7f28a317d7a221a79b36d524cd1aaa5c22ae0053
raihanuldev <rihanthedev@gmail.com>
2025-02-05 07:33:10 +0600

    bfs alg

--- Depth First Search (DFS) and 2D Grid mod-4/dfs_on_2D_grid.cpp
+++ Depth First Search (DFS) and 2D Grid mod-4/dfs_on_2D_grid.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 char grid[105][105];
+bool vis[105][105];
+vector<pair<int,int>>d = {{-1,0},{1,0},{0,-1},{0,1}};
+
+void dfs(int si,int sj)
+{
+    // cout<<si<<" "<<sj;
+    vis[si][sj] = true;
+    // source note theke left,right,up,down dhike jaoa jai tai ei ghula bahir korbo ekn
+    for(int i =0; i<4; i++)
+    {
+        int ci,cj;
+        ci =si+ d[i].first;
+        cj = sj+ d[i].second;
+        cout<<ci<<" "<<cj<<endl;
+    }
+}
 
 int main()
 {
@@ -11,11 +27,8 @@ int main()
         for(int j =0; j<m; j++)
             cin>>grid[i][j];
     
-    for(int i = 0; i<n;i++){
-        for(int j =0; j<m; j++){
-           cout<<grid[i][j];
-        }
-        cout<<endl;
-    }
+    int si,sj;
+    cin>>si>>sj;
+    dfs(si,sj);
     return 0;
 }
