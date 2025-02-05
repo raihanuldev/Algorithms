f34a07fcc32c0c9a3c45777a3db105121d76f066
raihanuldev <rihanthedev@gmail.com>
2025-02-05 11:29:58 +0600

    shortest-distance

--- Depth First Search (DFS) and 2D Grid mod-4/2d_shortes_distance_bfs.cpp
+++ Depth First Search (DFS) and 2D Grid mod-4/2d_shortes_distance_bfs.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 char grid[105][105];
 bool vis[105][105];
-bool level[105][105];
+int level[105][105];
 
 vector<pair<int,int>>d = {{-1,0},{1,0},{0,-1},{0,1}};
 int n,m;
@@ -13,7 +13,7 @@ bool isVaild(int i,int j)
         return false;
     return true;
 }
-void bfs(int si,int sj)
+void bfs(int si, int sj)
 {
     queue<pair<int,int>> q;
     q.push({si,sj});
@@ -25,35 +25,32 @@ void bfs(int si,int sj)
         q.pop();
         int par_i = par.first;
         int par_j = par.second;
-        // cout<<par_i<<" "<<par_j<<endl;
-        for(int i =0; i<4;i++)
+
+        for(int i=0;i<4;i++)
         {
             int ci = par_i + d[i].first;
             int cj = par_j + d[i].second;
-            if(isVaild(ci,cj) && !vis[ci][cj] && grid[ci][cj]=='.' ){
+            if(isVaild(ci,cj) && !vis[ci][cj] && grid[ci][cj]=='.')
+            {
                 q.push({ci,cj});
                 vis[ci][cj] = true;
-                level[ci][cj] = level[par_i][par_j] +1;
-                cout<<level[ci][cj]<<" "<< ci<<" "<<cj<<endl;
+                level[ci][cj] = level[par_i][par_j] + 1;
             }
         }
     }
 }
-
 int main()
 {
-    cin>>n>>m;
-    for(int i = 0; i<n;i++)
-        for(int j =0; j<m; j++)
-            cin>>grid[i][j];
-    
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++)
+            cin >> grid[i][j];
+
     int si,sj,di,dj;
-    cin>>si>>sj >> di >> dj;
-    
+    cin >> si >> sj >> di >> dj;
     memset(vis,false,sizeof(vis));
     memset(level,-1,sizeof(level));
     bfs(si,sj);
-    // cout<<di<<dj;
-    cout<<"Shortest Distance => "<<level[di][dj]<<endl;
+    cout << level[di][dj] << endl;
     return 0;
 }
