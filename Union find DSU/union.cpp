a1506e06643bb4af558a082ae6c3730b3e7c97d9
raihanuldev <rihanthedev@gmail.com>
2025-02-26 08:37:59 +0600

    union

---
+++ Union find DSU/union.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int par[100];
+int group_size[100];
+
+int find(int node)
+{
+    if(par[node]== -1)
+        return node;
+    int leader = find(par[node]);
+    par[node] = leader;
+    return leader;
+}
+void dsu_union(int node1,int node2)
+{
+    int leader1 = find(node1);
+    int leader2 = find(node2);
+    if(group_size[leader1]>=group_size[leader2]){
+        par[leader2] = leader1;
+        group_size[leader1]+=group_size[leader2];
+    }else{
+        par[leader1] = leader2;
+        group_size[leader2] +=group_size[leader1];
+    }
+}
+int main()
+{
+    memset(par,-1,sizeof(par));
+    memset(group_size,1,sizeof(group_size));
+    //nijer hate union korai disse
+    dsu_union(1,2);
+    for(int i =0; i<6;i++)
+    {
+        cout<<i<<" => "<<par[i]<<endl;
+    }
+    return 0;
+}
